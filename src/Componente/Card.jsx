d65c78df296401a0403832aa9a624e01c2fb92ba
rmora18 <ruthmoram18@gmail.com>
2023-05-31 14:19:20 -0300

    actualizando repo 1

---
+++ src/Componente/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div  
+    style={{width: '300px',
+        height: '400px',
+        border: '3px solid rgb(89, 57, 116)',
+        backgroundColor: 'rgb(35, 49, 81)',
+        marginBottom: '10px',}}>
+        <h2>
+        Hola soy una tarjeta
+        </h2>
+    </div>
+  )
+}
+
+export default Card
